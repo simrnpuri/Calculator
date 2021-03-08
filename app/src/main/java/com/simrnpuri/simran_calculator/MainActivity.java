9728ba008c7a5242d0eec29986d4d4a9187a2220
Simrn Puri <purisimran13@gmail.com>
2021-03-08 23:37:54 +0530

    layout commit

---
+++ app/src/main/java/com/simrnpuri/simran_calculator/MainActivity.java
@@ -0,0 +1,14 @@
+package com.simrnpuri.simran_calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
