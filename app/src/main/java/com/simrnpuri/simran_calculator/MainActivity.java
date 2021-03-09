d7c59a92b65768f572227009e04dccb9bbea7c0c
Simrn Puri <purisimran13@gmail.com>
2021-03-09 06:18:39 +0530

    Button Functions commit 1

--- app/src/main/java/com/simrnpuri/simran_calculator/MainActivity.java
+++ app/src/main/java/com/simrnpuri/simran_calculator/MainActivity.java
@@ -1,14 +1,449 @@
 package com.simrnpuri.simran_calculator;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.math.BigDecimal;
+//import javax.script.ScriptEngineManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
+{
+
+    private int openParenthesis = 0;
+    private boolean dotUsed = false;
+    private boolean equalClicked = false;
+    private String lastExpression = "";
+    private final static int EXCEPTION = -1;
+    private final static int IS_NUMBER = 0;
+    private final static int IS_OPERAND = 1;
+    private final static int IS_OPEN_PARENTHESIS = 2;
+    private final static int IS_CLOSE_PARENTHESIS = 3;
+    private final static int IS_DOT = 4;
+
+    Button button0;
+    Button button1;
+    Button button2;
+    Button button3;
+    Button button4;
+    Button button5;
+    Button button6;
+    Button button7;
+    Button button8;
+    Button button9;
+    Button buttonClear;
+    Button buttonAllClear;
+    Button buttonPercent;
+    Button buttonDivision;
+    Button buttonMultiplication;
+    Button buttonSubtraction;
+    Button buttonAddition;
+    Button buttonEqual;
+    Button buttonDecimal;
+    TextView textViewDisplay;
+
+    //ScriptEngine scriptEngine;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //scriptEngine = new ScriptEngineManager().getEngineByName("rhino");
     }
+
+    private void initializeViewVariables()
+    {
+        button0 = (Button) findViewById(R.id.button_zero);
+        button1 = (Button) findViewById(R.id.button_one);
+        button2 = (Button) findViewById(R.id.button_two);
+        button3 = (Button) findViewById(R.id.button_three);
+        button4 = (Button) findViewById(R.id.button_four);
+        button5 = (Button) findViewById(R.id.button_five);
+        button6 = (Button) findViewById(R.id.button_six);
+        button7 = (Button) findViewById(R.id.button_seven);
+        button8 = (Button) findViewById(R.id.button_eight);
+        button9 = (Button) findViewById(R.id.button_nine);
+        buttonClear = (Button) findViewById(R.id.button_clear);
+        buttonAllClear = (Button) findViewById(R.id.button_parentheses);
+        buttonPercent = (Button) findViewById(R.id.button_percent);
+        buttonDivision = (Button) findViewById(R.id.button_division);
+        buttonMultiplication = (Button) findViewById(R.id.button_multiplication);
+        buttonSubtraction = (Button) findViewById(R.id.button_subtraction);
+        buttonAddition = (Button) findViewById(R.id.button_addition);
+        buttonEqual = (Button) findViewById(R.id.button_equal);
+        buttonDecimal = (Button) findViewById(R.id.button_decimal);
+        textViewDisplay = (TextView) findViewById(R.id.textView_input_numbers);
+    }
+
+    private void setOnClickListeners()
+    {
+        button0.setOnClickListener(this);
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+        button4.setOnClickListener(this);
+        button5.setOnClickListener(this);
+        button6.setOnClickListener(this);
+        button7.setOnClickListener(this);
+        button8.setOnClickListener(this);
+        button9.setOnClickListener(this);
+        buttonClear.setOnClickListener(this);
+        buttonAllClear.setOnClickListener(this);
+        buttonPercent.setOnClickListener(this);
+        buttonDivision.setOnClickListener(this);
+        buttonMultiplication.setOnClickListener(this);
+        buttonSubtraction.setOnClickListener(this);
+        buttonAddition.setOnClickListener(this);
+        buttonEqual.setOnClickListener(this);
+        buttonDecimal.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view)
+    {
+        switch (view.getId())
+        {
+            case R.id.button_zero:
+                if (addNumber("0")) equalClicked = false;
+                break;
+            case R.id.button_one:
+                if (addNumber("1")) equalClicked = false;
+                break;
+            case R.id.button_two:
+                if (addNumber("2")) equalClicked = false;
+                break;
+            case R.id.button_three:
+                if (addNumber("3")) equalClicked = false;
+                break;
+            case R.id.button_four:
+                if (addNumber("4")) equalClicked = false;
+                break;
+            case R.id.button_five:
+                if (addNumber("5")) equalClicked = false;
+                break;
+            case R.id.button_six:
+                if (addNumber("6")) equalClicked = false;
+                break;
+            case R.id.button_seven:
+                if (addNumber("7")) equalClicked = false;
+                break;
+            case R.id.button_eight:
+                if (addNumber("8")) equalClicked = false;
+                break;
+            case R.id.button_nine:
+                if (addNumber("9")) equalClicked = false;
+                break;
+            case R.id.button_addition:
+                if (addOperand("+")) equalClicked = false;
+                break;
+            case R.id.button_subtraction:
+                if (addOperand("-")) equalClicked = false;
+                break;
+            case R.id.button_multiplication:
+                if (addOperand("x")) equalClicked = false;
+                break;
+            case R.id.button_division:
+                if (addOperand("/")) equalClicked = false;
+                break;
+            case R.id.button_percent:
+                if (addOperand("%")) equalClicked = false;
+                break;
+            case R.id.button_decimal:
+                if (addDot()) equalClicked = false;
+                break;
+            case R.id.button_parentheses:
+                if (addParenthesis()) equalClicked = false;
+                break;
+            case R.id.button_clear:
+                textViewDisplay.setText("");
+                openParenthesis = 0;
+                dotUsed = false;
+                equalClicked = false;
+                break;
+            case R.id.button_equal:
+                if (textViewDisplay.getText().toString() != null && !textViewDisplay.getText().toString().equals(""))
+                    calculate(textViewDisplay.getText().toString());
+                break;
+        }
+
+    }
+
+    private boolean addDot()
+    {
+        boolean done = false;
+
+        if (textViewDisplay.getText().length() == 0)
+        {
+            textViewDisplay.setText("0.");
+            dotUsed = true;
+            done = true;
+        } else if (dotUsed == true)
+        {
+        } else if (defineLastCharacter(textViewDisplay.getText().charAt(textViewDisplay.getText().length() - 1) + "") == IS_OPERAND)
+        {
+            textViewDisplay.setText(textViewDisplay.getText() + "0.");
+            done = true;
+            dotUsed = true;
+        } else if (defineLastCharacter(textViewDisplay.getText().charAt(textViewDisplay.getText().length() - 1) + "") == IS_NUMBER)
+        {
+            textViewDisplay.setText(textViewDisplay.getText() + ".");
+            done = true;
+            dotUsed = true;
+        }
+        return done;
+    }
+
+    private boolean addParenthesis()
+    {
+        boolean done = false;
+        int operationLength = textViewDisplay.getText().length();
+
+        if (operationLength == 0)
+        {
+            textViewDisplay.setText(textViewDisplay.getText() + "(");
+            dotUsed = false;
+            openParenthesis++;
+            done = true;
+        } else if (openParenthesis > 0 && operationLength > 0)
+        {
+            String lastInput = textViewDisplay.getText().charAt(operationLength - 1) + "";
+            switch (defineLastCharacter(lastInput))
+            {
+                case IS_NUMBER:
+                    textViewDisplay.setText(textViewDisplay.getText() + ")");
+                    done = true;
+                    openParenthesis--;
+                    dotUsed = false;
+                    break;
+                case IS_OPERAND:
+                    textViewDisplay.setText(textViewDisplay.getText() + "(");
+                    done = true;
+                    openParenthesis++;
+                    dotUsed = false;
+                    break;
+                case IS_OPEN_PARENTHESIS:
+                    textViewDisplay.setText(textViewDisplay.getText() + "(");
+                    done = true;
+                    openParenthesis++;
+                    dotUsed = false;
+                    break;
+                case IS_CLOSE_PARENTHESIS:
+                    textViewDisplay.setText(textViewDisplay.getText() + ")");
+                    done = true;
+                    openParenthesis--;
+                    dotUsed = false;
+                    break;
+            }
+        } else if (openParenthesis == 0 && operationLength > 0)
+        {
+            String lastInput = textViewDisplay.getText().charAt(operationLength - 1) + "";
+            if (defineLastCharacter(lastInput) == IS_OPERAND)
+            {
+                textViewDisplay.setText(textViewDisplay.getText() + "(");
+                done = true;
+                dotUsed = false;
+                openParenthesis++;
+            } else
+            {
+                textViewDisplay.setText(textViewDisplay.getText() + "x(");
+                done = true;
+                dotUsed = false;
+                openParenthesis++;
+            }
+        }
+        return done;
+    }
+
+    private boolean addOperand(String operand)
+    {
+        boolean done = false;
+        int operationLength = textViewDisplay.getText().length();
+        if (operationLength > 0)
+        {
+            String lastInput = textViewDisplay.getText().charAt(operationLength - 1) + "";
+
+            if ((lastInput.equals("+") || lastInput.equals("-") || lastInput.equals("*") || lastInput.equals("\u00F7") || lastInput.equals("%")))
+            {
+                Toast.makeText(getApplicationContext(), "Wrong format", Toast.LENGTH_LONG).show();
+            } else if (operand.equals("%") && defineLastCharacter(lastInput) == IS_NUMBER)
+            {
+                textViewDisplay.setText(textViewDisplay.getText() + operand);
+                dotUsed = false;
+                equalClicked = false;
+                lastExpression = "";
+                done = true;
+            } else if (!operand.equals("%"))
+            {
+                textViewDisplay.setText(textViewDisplay.getText() + operand);
+                dotUsed = false;
+                equalClicked = false;
+                lastExpression = "";
+                done = true;
+            }
+        } else
+        {
+            Toast.makeText(getApplicationContext(), "Wrong Format. Operand Without any numbers?", Toast.LENGTH_LONG).show();
+        }
+        return done;
+    }
+
+    private boolean addNumber(String number)
+    {
+        boolean done = false;
+        int operationLength = textViewDisplay.getText().length();
+        if (operationLength > 0)
+        {
+            String lastCharacter = textViewDisplay.getText().charAt(operationLength - 1) + "";
+            int lastCharacterState = defineLastCharacter(lastCharacter);
+
+            if (operationLength == 1 && lastCharacterState == IS_NUMBER && lastCharacter.equals("0"))
+            {
+                textViewDisplay.setText(number);
+                done = true;
+            } else if (lastCharacterState == IS_OPEN_PARENTHESIS)
+            {
+                textViewDisplay.setText(textViewDisplay.getText() + number);
+                done = true;
+            } else if (lastCharacterState == IS_CLOSE_PARENTHESIS || lastCharacter.equals("%"))
+            {
+                textViewDisplay.setText(textViewDisplay.getText() + "x" + number);
+                done = true;
+            } else if (lastCharacterState == IS_NUMBER || lastCharacterState == IS_OPERAND || lastCharacterState == IS_DOT)
+            {
+                textViewDisplay.setText(textViewDisplay.getText() + number);
+                done = true;
+            }
+        } else
+        {
+            textViewDisplay.setText(textViewDisplay.getText() + number);
+            done = true;
+        }
+        return done;
+    }
+
+
+    private void calculate(String input)
+    {
+        String result = "";
+        try
+        {
+            String temp = input;
+            if (equalClicked)
+            {
+                temp = input + lastExpression;
+            } else
+            {
+                saveLastExpression(input);
+            }
+            //result = scriptEngine.eval(temp.replaceAll("%", "/100").replaceAll("x", "*").replaceAll("[^\\x00-\\x7F]", "/")).toString();
+            BigDecimal decimal = new BigDecimal(result);
+            result = decimal.setScale(8, BigDecimal.ROUND_HALF_UP).toPlainString();
+            equalClicked = true;
+
+        } catch (Exception e)
+        {
+            Toast.makeText(getApplicationContext(), "Wrong Format", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (result.equals("Infinity"))
+        {
+            Toast.makeText(getApplicationContext(), "Division by zero is not allowed", Toast.LENGTH_SHORT).show();
+            textViewDisplay.setText(input);
+
+        } else if (result.contains("."))
+        {
+            result = result.replaceAll("\\.?0*$", "");
+            textViewDisplay.setText(result);
+        }
+    }
+
+    private void saveLastExpression(String input)
+    {
+        String lastOfExpression = input.charAt(input.length() - 1) + "";
+        if (input.length() > 1)
+        {
+            if (lastOfExpression.equals(")"))
+            {
+                lastExpression = ")";
+                int numberOfCloseParenthesis = 1;
+
+                for (int i = input.length() - 2; i >= 0; i--)
+                {
+                    if (numberOfCloseParenthesis > 0)
+                    {
+                        String last = input.charAt(i) + "";
+                        if (last.equals(")"))
+                        {
+                            numberOfCloseParenthesis++;
+                        } else if (last.equals("("))
+                        {
+                            numberOfCloseParenthesis--;
+                        }
+                        lastExpression = last + lastExpression;
+                    } else if (defineLastCharacter(input.charAt(i) + "") == IS_OPERAND)
+                    {
+                        lastExpression = input.charAt(i) + lastExpression;
+                        break;
+                    } else
+                    {
+                        lastExpression = "";
+                    }
+                }
+            } else if (defineLastCharacter(lastOfExpression + "") == IS_NUMBER)
+            {
+                lastExpression = lastOfExpression;
+                for (int i = input.length() - 2; i >= 0; i--)
+                {
+                    String last = input.charAt(i) + "";
+                    if (defineLastCharacter(last) == IS_NUMBER || defineLastCharacter(last) == IS_DOT)
+                    {
+                        lastExpression = last + lastExpression;
+                    } else if (defineLastCharacter(last) == IS_OPERAND)
+                    {
+                        lastExpression = last + lastExpression;
+                        break;
+                    }
+                    if (i == 0)
+                    {
+                        lastExpression = "";
+                    }
+                }
+            }
+        }
+    }
+
+    private int defineLastCharacter(String lastCharacter)
+    {
+        try
+        {
+            Integer.parseInt(lastCharacter);
+            return IS_NUMBER;
+        } catch (NumberFormatException e)
+        {
+        }
+
+        if ((lastCharacter.equals("+") || lastCharacter.equals("-") || lastCharacter.equals("x") || lastCharacter.equals("\u00F7") || lastCharacter.equals("%")))
+            return IS_OPERAND;
+
+        if (lastCharacter.equals("("))
+            return IS_OPEN_PARENTHESIS;
+
+        if (lastCharacter.equals(")"))
+            return IS_CLOSE_PARENTHESIS;
+
+        if (lastCharacter.equals("."))
+            return IS_DOT;
+
+        return -1;
+    }
+
+
 }
